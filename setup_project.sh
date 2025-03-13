c5fafed158a96e540bc82eb93b983f8941b88e9a
Manikandan <manikandanperumal00@gmail.com>
2025-03-13 10:17:39 +0000

    Added setup_project.sh

--- setup_project.sh
+++ setup_project.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-set -e
-
-REPO_URL="git@github.com:Manikandan78/Nix_package_project.git"
-INSTALL_DIR="$HOME/nix_project"
-
-echo "🔹 Cloning repository..."
-if [ -d "$INSTALL_DIR" ]; then
-    echo "⚠️ Repository already exists. Pulling latest updates..."
-    cd "$INSTALL_DIR" && git pull
-else
-    git clone "$REPO_URL" "$INSTALL_DIR"
-    cd "$INSTALL_DIR" || exit 1
-fi
 
 echo "🔹 Setting up virtual environment..."
 python3 -m venv venv
