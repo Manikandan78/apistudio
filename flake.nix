bf3c92e09196aa96efa6563a556308f83edcf737
mani123653 <t978732372gmail.com>
2025-04-17 09:38:02 +0000

    Clean state

--- flake.nix
+++ flake.nix
@@ -1,36 +1,41 @@
 {
-  description = "API Studio full deployment environment with Python, PostgreSQL, pgAdmin, Nginx, and Certbot";
+  description = "API Studio with Nginx, PostgreSQL, and Certbot";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";  # Use recent Nixpkgs
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+        };
       in {
         devShell = pkgs.mkShell {
           name = "apistudio-env";
 
-buildInputs = with pkgs; [
-  python3
-  python3Packages.virtualenv
+          buildInputs = with pkgs; [
+            python3
+            python3Packages.virtualenv
 
-  postgresql
-  pgadmin4
+            postgresql
+            pgadmin4
 
-  nginx
+            nginx
 
-  (certbot.override {
-    plugins = [ certbotPlugins.nginx ];
-  })
+            certbot
+            certbot-nginx  # <-- This is the correct plugin name in nixpkgs 23.11+
 
-  bash
-  git
-  curl
+            bash
+            git
+            curl
           ];
+
+          shellHook = ''
+            echo "🚀 API Studio Dev Shell Ready!"
+          '';
         };
       });
 }
